b260e122bf9ee58640b81fcdcfb21757d12f9252
manishagithub22 <mpokhrel363@gmail.com>
2024-01-18 14:32:34 +0545

    SImple C Programto Print Hello World

---
+++ 01.First-Semester/01.C-Programming/01.Basics/01.demo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+// Here, stdio.h and conio.h are header files which are linked in this file
+
+int main()
+{
+    printf("Hello, World");
+    // printf displays the output on the screen......
+    return 0;
+}
